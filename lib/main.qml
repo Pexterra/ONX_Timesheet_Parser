395a2ea6c569cc0a55d376d34b42da5deca5af2f
Pexterra <exeterar@gmail.com>
2024-04-17 01:15:32 +0200

    initial commit

---
+++ lib/main.qml
@@ -0,0 +1,100 @@
+import QtQuick 2.0
+import QtQuick.Layouts 1.11
+import QtQuick.Controls 2.1
+import QtQuick.Window 2.1
+import QtQuick.Controls.Material 2.1
+import QtQuick.Dialogs
+
+import io.qt.textproperties 1.0
+
+ApplicationWindow {
+    id: page
+    width: 900
+    height: 600
+    minimumHeight: 600
+    minimumWidth: 900
+    visible: true
+    Material.theme: Material.Dark
+    Material.accent: Material.Purple
+    title: qsTr("ONX Timesheet Parser")
+
+    Timesheet {
+        id: timesheet
+    }
+
+    FileDialog {
+        id: fileDialog
+        onAccepted: {
+            output.text = "data compilation in progress...\n(might take some time depending on table size)"
+            timesheet.loadCSV(selectedFile)
+            output.text = timesheet.getTimesheet()
+            citizenSelection.model = timesheet.getPlayers()
+            citizenSelection.visible = true
+            citizenSelectionLabel.visible = true
+        }
+    }
+    RowLayout {
+        anchors.fill: parent
+        ColumnLayout {
+            Layout.alignment: Qt.AlignTop
+            Layout.maximumWidth: 300
+            Layout.minimumWidth: 300
+            spacing: 10
+            ComboBox {
+                id: timezones
+                Layout.leftMargin: 10
+                Layout.topMargin: 10
+                Layout.alignment: Qt.AlignHCenter
+                Layout.preferredWidth: parent.width
+                model: ['UTC+14:00', 'UTC+13:00', 'UTC+12:45', 'UTC+12:00','UTC+11:00', 'UTC+10:30', 'UTC+10:00', 'UTC+09:30', 'UTC+09:00', 'UTC+08:45', 'UTC+08:00', 
+                'UTC+07:00', 'UTC+06:30', 'UTC+06:00', 'UTC+05:45', 'UTC+05:30', 'UTC+05:00', 'UTC+04:00', 'UTC+03:30', 'UTC+03:00', 'UTC+02:00', 'UTC+01:00',
+                'UTC-00:00', 'UTC-01:00', 'UTC-02:00', 'UTC-03:00', 'UTC-03:30', 'UTC-04:00', 'UTC-05:00', 'UTC-06:00', 'UTC-07:00', 'UTC-08:00', 'UTC-09:00', 
+                'UTC-09:30', 'UTC-10:00', 'UTC-11:00', 'UTC-12:00' ]
+                currentIndex: 22
+                onActivated: {
+                    timesheet.setTimezone(currentText)
+                }
+            }
+            Button {
+                Layout.alignment: Qt.AlignTop | Qt.AlignHCenter
+                text: qsTr("Load Excel file...")
+                onClicked: {
+                    fileDialog.open()
+                }
+            }
+            Text {
+                id: citizenSelectionLabel
+                Layout.alignment: Qt.AlignHCenter
+                text: 'Select Player'
+                font.pixelSize: 15
+                color: "pink"
+                visible: false
+            }
+            ComboBox {
+                id: citizenSelection
+                Layout.leftMargin: 10
+                Layout.alignment: Qt.AlignHCenter
+                Layout.preferredWidth: parent.width
+                model: []
+                onActivated: {
+                    output.text = timesheet.getPlayerData(currentText)
+                }
+                visible: false
+            }
+        }
+        Flickable {
+            id: flickable
+            Layout.fillWidth: true
+            Layout.fillHeight: true
+            Layout.leftMargin: 20
+
+            TextArea.flickable: TextArea {
+                id: output
+                text: ""
+                wrapMode: TextArea.Wrap
+            }
+
+            ScrollBar.vertical: ScrollBar { }
+        }
+    }
+}
---
+++ lib/timesheetParser.py
@@ -0,0 +1,100 @@
+import sys, datetime, pandas as pd
+
+from datetime import timedelta
+from PySide6.QtCore import QObject, Slot
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine, QmlElement
+from PySide6.QtQuickControls2 import QQuickStyle
+
+QML_IMPORT_NAME = "io.qt.textproperties"
+QML_IMPORT_MAJOR_VERSION = 1
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.loggedIn = False
+        self.loggedTime = datetime.timedelta(0)
+        self.logins = []
+        self.logouts = []
+
+@QmlElement
+class Timesheet(QObject):
+    def __init__(self):
+        super().__init__()
+        self.timesheetString = ""
+        self.displayedPlayers = []
+
+    @Slot(str)
+    def loadCSV(self, file):
+        self.timesheetString = ""
+        self.displayedPlayers = []
+        xls = pd.ExcelFile(file)
+        timezone = xls.parse('Actions', index_col=3, )
+        df = xls.parse('Actions', skiprows=3, skipcolumns=1)
+        self.players : Player = []
+        registered = []
+        for name in df.Name:
+            if name not in registered:
+                registered.append(name)
+                self.players.append(Player(name))
+
+        for i in range(0, len(df.Action)):
+            for player in self.players:
+                if df.Name[i] == player.name:
+                    if df.Action[i] == 'Check In' and player.loggedIn == False:
+                        player.logins.append(df.Time[i] + timedelta(hours=self.timezone))
+                        player.loggedIn = True
+                        break
+                    if df.Action[i] == 'Check Out' and player.loggedIn == True:
+                        player.logouts.append(df.Time[i]+ timedelta(hours=self.timezone))
+                        player.loggedIn = False
+                        break
+
+        for player in self.players:
+            for i in range(0, min(len(player.logins),len(player.logouts))):
+                player.loggedTime = player.loggedTime + (player.logouts[i] - player.logins[i])
+
+        self.players.sort(key=lambda x: x.loggedTime, reverse=True)
+        for player in self.players:
+            if player.loggedTime > datetime.timedelta(0):
+                self.timesheetString +=  "{}     {}".format(str(player.loggedTime).rjust(17),player.name) + '\n'
+                self.displayedPlayers.append(player.name)
+        self.displayedPlayers.sort()
+        self.displayedPlayers.insert(0, "Overview")
+
+    @Slot(str)
+    def setTimezone(self, timezone):
+        self.timezone = int(timezone[-6:-3])
+
+    @Slot(result=str)
+    def getTimesheet(self):
+        return self.timesheetString
+
+    @Slot(result=list)
+    def getPlayers(self):
+        return self.displayedPlayers
+
+    @Slot(str, result=str)
+    def getPlayerData(self, playerSelection):
+        if playerSelection == 'Overview':
+            return self.timesheetString
+        else:
+            output = ""
+            for player in self.players:
+                if player.name == playerSelection:
+                    output += "{} - clocked time: {}".format(player.name, str(player.loggedTime).rjust(17)) + '\n\n'
+                    output += f"UTC{self.timezone}" + '\n'
+                    for i in range(0, min(len(player.logins),len(player.logouts))):
+                        output +=  "in: {}  -  out: {}".format(str(player.logins[i]),str(player.logouts[i])) + '\n'
+            return output
+
+    
+if __name__ == '__main__':
+    app = QGuiApplication(sys.argv)
+    QQuickStyle.setStyle("Material")
+    engine = QQmlApplicationEngine()
+
+    qml_file = 'lib/main.qml'
+    engine.load(qml_file)
+
+    sys.exit(app.exec())
