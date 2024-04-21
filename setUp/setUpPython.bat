a7407dc1981a23ae9a26e6902fbc4505b813a97f
Pexterra <exeterar@gmail.com>
2024-04-21 22:05:44 +0200

    start_app.bat now installs required packages by default setUp/setUpPython.bat doesn't

--- setUp/setUpPython.bat
+++ setUp/setUpPython.bat
@@ -3,8 +3,3 @@ title Installing required files
 
 echo Installing/Updating Python
 "./python_3116.exe" /passive /quite
-
-
-ECHO Installing required python packages via pip [any key to confirm, otherwise close cmd]
-set /p keyreceived=
-python -m pip install -r ../requirements.txt
